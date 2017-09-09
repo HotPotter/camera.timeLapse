f624b561f069e8ac98070f91728e270793c2da1a
Yan Dou <HotPotter@users.noreply.github.com>
2017-09-09 19:28:43 +0200

    add makeGif script and photos

---
+++ makeGIF.py
@@ -0,0 +1,40 @@
+import sys
+import datetime
+import imageio
+import os, glob
+import shutil
+VALID_EXTENSIONS = ('png', 'jpg')
+
+
+def create_gif(filenames, duration):
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(filename))
+    output_file = 'GIFs/avocado.gif'
+    imageio.mimwrite(output_file, images, duration=duration)
+
+
+def main():
+    args = sys.argv
+    script = args.pop(0)
+
+    if len(args) < 1:
+        print( 'Usage: python {} <duration> <path to folder>'.format(script))
+        sys.exit(1)
+
+    duration = float(args[0])
+    input_dir = args[1]
+    filenames = [os.path.join(input_dir, i )for i in os.listdir(input_dir)]
+
+    if not all(f.lower().endswith(VALID_EXTENSIONS) for f in filenames):
+        print('Only png and jpg files allowed')
+        sys.exit(1)
+
+    create_gif(filenames, duration)
+
+
+if __name__ == "__main__":
+    main()
+    print("done")
+
+
