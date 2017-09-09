1d2f3fccdab61a8a29995f3e3027ddcd410515a6
Philipp Eisen <philipp.eisen@king.com>
2017-09-09 19:41:16 +0200

    created script that makes photos

---
+++ make_photos.py
@@ -0,0 +1,27 @@
+from picamera import PiCamera
+import sys
+import time
+import os
+
+def take_photo(output_dir):
+    with PiCamera() as picam:
+        filename = '{}.jpg'.format(round(time.time()))
+        output_path = os.path.join(output_dir, filename)
+        picam.capture(output_path, format='jpeg')
+
+def main(args):
+    script = args.pop(0)
+
+    if len(args) < 2:
+        print('Usage: python {} <photo_every_x_seconds> <output_dir>'.format(script))
+
+    interval = int(args[0])
+    output_dir = args[1]
+
+    while True:
+       take_photo(output_dir)
+       time.sleep(interval)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
