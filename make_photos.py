4454511d2d570e8c5610df1ece44dcb3878f5d3d
Philipp Eisen <philipp.eisen@king.com>
2017-09-09 22:25:45 +0200

    added rotation

--- make_photos.py
+++ make_photos.py
@@ -7,13 +7,14 @@ def take_photo(output_dir):
     with PiCamera() as picam:
         filename = '{}.jpg'.format(round(time.time()))
         output_path = os.path.join(output_dir, filename)
-        picam.capture(output_path, format='jpeg')
+        picam.capture(output_path, format='jpeg', rotation=180)
 
 def main(args):
     script = args.pop(0)
 
     if len(args) < 2:
         print('Usage: python {} <photo_every_x_seconds> <output_dir>'.format(script))
+        exit(1)
 
     interval = int(args[0])
     output_dir = args[1]
